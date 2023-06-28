e13b2d5346dea7dfdbb6d69378f05906a3feb7d7
Brandon Gompert <brandongompert@gmail.com>
2023-06-27 21:13:26 -0400

    camera roll pretty now :)

--- app/screens/CameraRollScreen.tsx
+++ app/screens/CameraRollScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View, Button, Image, Alert } from "react-native"
+import { View, Button, Image, Alert, HStack } from "native-base"
 import * as ImagePicker from "expo-image-picker"
 
 export const CameraRollScreen = () => {
@@ -9,10 +9,7 @@ export const CameraRollScreen = () => {
     try {
       const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
       if (status !== "granted") {
-        Alert.alert(
-          "Permission Denied",
-          "Sorry, we need camera roll permissions to select an image.",
-        )
+        Alert(["Permission Denied", "Sorry, we need camera roll permissions to select an image."])
         return
       }
 
@@ -26,36 +23,39 @@ export const CameraRollScreen = () => {
       if (!result.canceled) {
         console.log(result)
         setSelectedImage(result.assets[0].uri)
-        uploadImage(result.assets[0].uri)
       }
     } catch (error) {
       console.log("Error selecting image:", error)
+    }
+  }
+  const uploadImage = async (uri) => {
+    try {
+      const response = await fetch("YOUR_BACKEND_API_ENDPOINT", {
+        method: "POST",
+        body: uri,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+
+      const result = await response.json()
+      console.log("Image upload result:", result)
+      setSelectedImage(null)
+      return result
+    } catch (error) {
+      console.log("Error uploading image:", error)
     }
   }
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       {selectedImage && (
-        <Image source={{ uri: selectedImage }} style={{ width: 200, height: 200 }} />
+        <Image source={{ uri: selectedImage }} style={{ width: 300, height: 300 }} alt="Uh Oh" />
       )}
-      <Button title="Select Image" onPress={selectImage} />
+      <HStack space={6} pt={4}>
+        <Button onPress={selectImage}>Select Image</Button>
+        <Button onPress={uploadImage}>Upload Image</Button>
+      </HStack>
     </View>
   )
-}
-
-const uploadImage = async (uri) => {
-  try {
-    const response = await fetch("YOUR_BACKEND_API_ENDPOINT", {
-      method: "POST",
-      body: uri,
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    })
-
-    const result = await response.json()
-    console.log("Image upload result:", result)
-  } catch (error) {
-    console.log("Error uploading image:", error)
-  }
 }
